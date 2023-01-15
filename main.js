6cb7d5f8c01b86b132cb0d8e7233d9dc2304de89
Marek Praski <marekpraski@prgw.com.pl>
2023-01-15 18:56:52 +0100

    dodałem komentarze wyjaśniające znaczenie istotnych linii kodu

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const dateTools = require(__dirname + "/modules/dateTools.js");
+const dateTools = require(__dirname + "/modules/dateTools.js");     //użycie własnego modułu zawierającego funkcje
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine', 'ejs');
-app.use(express.static("public"));
+
+app.set('view engine', 'ejs');                      //ustawienia dla modułu ejs, nie kombinować, tak ma być, taka jest składnia
+app.use(express.static("public"));              //ustawienia katalogu, w którym są pliki css, obrazki i inne zasoby, które muszą być wykorzystane przez strony ejs
 
 var listItems = ["buy food", "cook food", "eat food"];
 
 app.get("/", (req, res)=>{
     let day = dateTools.date();
-    res.render("toDoList", {dayOfWeek: day, newItems: listItems});
+    res.render("toDoList", {dayOfWeek: day, newItems: listItems});  //używając modułu ejs pliki html zamieniam na ejs
 
 });
 
