738903d3f692c93b2f3ba686be04cd0e86c7c5bb
Marek Praski <marekpraski@prgw.com.pl>
2023-01-25 21:06:06 +0100

    do przechowywania elementów listy tworzę obiekty ListItem (poprzednio wrzucałem tylko tekst) po zaznaczeniu checkboxa listy element jest usuwany z listy; w aplikacji jest lista elementów ukończonych, element usunięty jest dodawany do tej listy; lista elementów do zrobienia i lista elementów ukończonych zapisywane są do plików tekstowych;

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dateTools = require(__dirname + "/modules/dateTools.js");     //użycie własnego modułu zawierającego funkcje
+const fs = require("fs");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -8,20 +9,49 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');                      //ustawienia dla modułu ejs, nie kombinować, tak ma być, taka jest składnia
 app.use(express.static("public"));              //ustawienia katalogu, w którym są pliki css, obrazki i inne zasoby, które muszą być wykorzystane przez strony ejs
 
-var listItems = ["buy food", "cook food", "eat food"];
+function readData(dataFile){
+    return JSON.parse(fs.readFileSync(dataFile, "utf8"));   //koniecznie Sync, inaczej idzie dalej i zmienna jest undefined !!!
+}
+
+function ListItem(value){
+    this.value = value;
+}
+
+
+var completedItems = readData("data/completedList.txt");;
+var listItems = readData("data/list.txt");
 
 app.get("/", (req, res)=>{
-    let day = dateTools.date();
+    let day = dateTools.day();
     res.render("toDoList", {dayOfWeek: day, newItems: listItems});  //używając modułu ejs pliki html zamieniam na ejs
-
 });
 
 app.post("/", (req, res)=>{
-    let i = req.body.newListItem;
-    listItems.push(i);
-    console.log(i);
+    let newItem = new ListItem(req.body.newListItem);
+    listItems.push(newItem);
+    saveData("data/list.txt", listItems);
     res.redirect("/");
-})
+});
+
+app.post("/delete", (req, res)=>{
+    removeItem(req.body.chb);
+    res.redirect("/");
+});
+
+function saveData(dataFile, items){
+    fs.writeFile(dataFile, JSON.stringify(items), (err)=>{
+        if(err){
+            console.log(err);
+        }
+    });
+}
+
+function removeItem(itemId){
+    completedItems.push(listItems[itemId]);
+    listItems.splice(itemId, 1);
+    saveData("data/completedList.txt", completedItems);
+    saveData("data/list.txt", listItems);
+}
 
 
 
